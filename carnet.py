1186b2e4c80487226c2c101c54284ff469440aa2
Richard Higgins <richard@relh.net>
2018-11-22 10:26:02 -0500

    version with 3 classes instead

--- carnet.py
+++ carnet.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import pickle
+import csv
 import numpy as np
 from skimage import io
 
@@ -15,6 +16,10 @@ from se_resnet import se_resnet_custom
 from utils import Trainer
 
 def get_all_image_label_pairs(root):
+    with open('classes.csv', 'r') as class_key:
+      reader = csv.reader(class_key)
+      list_mapping = list(reader)
+
     item = []
     for f in os.listdir(root):
         if os.path.isdir(os.path.join(root,f)):  
@@ -27,19 +32,19 @@ def get_all_image_label_pairs(root):
                       bbox_cols = [0]*10
 
                     # Append items to dataset
-                    item.append((os.path.join(root,f,ff), bbox_cols[9]))
+                    mod_val = int(list_mapping[int(bbox_cols[9])+1][-1])
+                    item.append((os.path.join(root,f,ff), mod_val))
 
                     # Commented out code for having the bounding box be the label
                     # Each row contains information of a bounding box: rotation vector, position (centroid x, y, z), size of the bounding box (length, width, height)
                     #item.append((os.path.join(root,f,ff), bbox_cols[0:9]))
-            break
     return item 
 
     
 class CarDataset(Dataset):
-    def __init__(self, root, transform):
+    def __init__(self, item_names, transform):
         """This Dataset takes in a folder root, the frequency with which to include samples in validation, and a flag for validation."""
-        self.item_names = get_all_image_label_pairs(root)
+        self.item_names = item_names
         self.transform = transform
         
     def __getitem__(self, index):
@@ -47,8 +52,6 @@ class CarDataset(Dataset):
         loaded_im = io.imread(im_path)
         trans_im = self.transform(loaded_im)
         trans_im.permute(2,0,1) 
-        # TODO what transformations are needed
-        #print(trans_im)
         return im_path, torch.tensor(trans_im).float(), torch.from_numpy(np.array(im_class)).long()
 
     def __len__(self):
@@ -74,7 +77,8 @@ def get_dataloader(batch_size, root, split_size=0.75):
     val_step = 4
 
     # Create the datasets
-    carData = CarDataset(root, to_normalized_tensor)
+    item_names = get_all_image_label_pairs(root)
+    carData = CarDataset(item_names, to_normalized_tensor)
     train_len = int(len(carData) * split_size)
     val_len = len(carData) - train_len 
     print("--- Train Length: {} ---".format(train_len))
@@ -87,7 +91,7 @@ def get_dataloader(batch_size, root, split_size=0.75):
     return train_loader, val_loader
 
 
-def main(batch_size, root, lr):
+def main(batch_size, root, lr, load, load_epoch, train, testing):
     # Get the train and validation data loaders
     train_loader, test_loader = get_dataloader(batch_size, root)
 
@@ -96,8 +100,14 @@ def main(batch_size, root, lr):
     print('--- GPUS: {} ---'.format(str(gpus)))
 
     # Build the model to run
-    se_resnet = nn.DataParallel(se_resnet_custom(num_classes=23), device_ids=gpus)
+    se_resnet = nn.DataParallel(se_resnet_custom(num_classes=3), device_ids=gpus)
     #se_resnet = se_resnet20(num_classes=23)#, device_ids=torch.device("cpu"))
+
+    if load:
+      details = torch.load("model_epoch_{}.pth".format(str(load_epoch)))
+      #new_details = dict([(k[7:], v) for k, v in details['weight'].items()])
+      #set_trace()
+      se_resnet.load_state_dict(details['weight'])
 
     # Declare the optimizer, learning rate scheduler, and training loops. Note that models are saved to the current directory.
     #optimizer = optim.SGD(params=se_resnet.parameters(), lr=lr, momentum=0.9, weight_decay=1e-4)
@@ -105,27 +115,25 @@ def main(batch_size, root, lr):
     scheduler = optim.lr_scheduler.StepLR(optimizer, 30, gamma=0.1)
     trainer = Trainer(se_resnet, optimizer, F.cross_entropy, save_dir=".")
 
-    train = True 
     if train:
       trainer.loop(100, train_loader, test_loader, scheduler)
 
-    testing = True
     if testing:
-      details = torch.load("new_models/model_epoch_9.pth")
+      '''details = torch.load("new_models/model_epoch_9.pth")
       new_details = dict([(k[7:], v) for k, v in details['weight'].items()])
       se_resnet = se_resnet_custom(num_classes=23)
-      se_resnet.load_state_dict(new_details)
+      se_resnet.load_state_dict(new_details)'''
       se_resnet.eval()
       t_l_1, t_l_2 = get_dataloader(batch_size, '/hdd/test/', 1.0)
       outputs = trainer.test(t_l_1)
-      with open('classes.csv', 'r') as class_key:
-        reader = csv.reader(class_key)
-        list_mapping = list(reader)
+      #with open('classes.csv', 'r') as class_key:
+      #  reader = csv.reader(class_key)
+      #  list_mapping = list(reader)
       with open('submission.csv', 'w') as sub:
         sub.write('guid/image,label\n')
         for name, val in outputs:
           mod_name = name[0].split('/')[3] + '/' + name[0].split('/')[4].split('_')[0]
-          mod_val = list_mapping[int(val)+1][-1]
+          mod_val = int(val) #list_mapping[int(val)+1][-1]
           sub.write(mod_name + ',' + str(mod_val) + '\n')
       print('done!')
 
@@ -137,5 +145,9 @@ if __name__ == '__main__':
     p.add_argument("--root", default='/hdd/trainval/', type=str, help="carnet data root")
     p.add_argument("--batch_size", default=1, type=int, help="batch size")
     p.add_argument("--lr", default=1e-3, type=float, help="learning rate")
+    p.add_argument("--load", default=False, type=bool, help="whether to load a model")
+    p.add_argument("--load_epoch", default=4, type=int, help="what epoch to load")
+    p.add_argument("--train", default=True, type=bool, help="whether to train a model")
+    p.add_argument("--test", default=True, type=bool, help="whether to test a model")
     args = p.parse_args()
-    main(args.batch_size, args.root, args.lr)
+    main(args.batch_size, args.root, args.lr, args.load, args.load_epoch, args.train, args.test)
