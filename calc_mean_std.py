5e7029754d5d70795c491ab88bad7ff3234d3bb2
Richard Higgins <richard@relh.net>
2018-11-22 23:01:23 -0500

    making a sample task2 submission

--- calc_mean_std.py
+++ calc_mean_std.py
@@ -2,12 +2,37 @@ import torch
 import os
 import numpy as np
 from skimage import io
+from PIL import Image
 from pdb import set_trace
 from torch.utils.data import Dataset, DataLoader
 
-from carnet import CarDataset 
+class CarDataset(Dataset):
+    def __init__(self, item_names, transform=None):
+        """This Dataset takes in a folder root, the frequency with which to include samples in validation, and a flag for validation."""
+        self.item_names = item_names
+        self.transform = transform
+        
+    def __getitem__(self, index):
+        im_path, im_class = self.item_names[index]
+        loaded_im = io.imread(im_path)
+        #loaded_im = Image.open(im_path)
+        #trans_im = self.transform(loaded_im)
+        #loaded_im = loaded_im.permute(2,0,1) 
+        return im_path, torch.tensor(loaded_im).float(), torch.from_numpy(np.array(im_class)).long()
 
-def get_all_images(base):
+    def __len__(self):
+        return len(self.item_names)
+
+
+def _get_dataloader(batch_size, dataset):
+    return DataLoader(
+        dataset,
+        batch_size=batch_size,
+        num_workers=0,
+        shuffle=True
+    )
+
+def get_all_image_label_pairs(base):
     """This function collects image names and their associated labels from a directory."""
     item = []
     for f in os.listdir(base):
@@ -18,7 +43,7 @@ def get_all_images(base):
                     bbox_cols = np.fromfile(os.path.join(base,f,root+'_bbox.bin'), dtype=np.float32)
                     #proj_cols = np.fromfile(os.path.join(base,f,root+'_proj.bin'), dtype=np.float32)
                     #cloud_cols = np.fromfile(os.path.join(base,f,root+'_cloud.bin'), dtype=np.float32)
-                    item.append((os.path.join(base,f,ff), bbox_cols[9]))
+                    item.append((os.path.join(base,f,ff), bbox_cols[0:9]))
     return item 
 
     
@@ -31,16 +56,14 @@ def compute_mean_std(loader):
     for i, data_tup in enumerate(loader):
         print("{} / {}".format(i, len(loader)))
         
-        data = data_tup[0]
+        data = data_tup[2][:, 3:6]
         batch_samples = data.size(0)
         data = data.float()
       	#data = data.view(batch_samples, data.size(1), -1)
         
-        mean += torch.mean(torch.mean(data, 1), 1)
-        std += torch.mean(torch.std(data, 1), 1)
+        mean += torch.mean(data, 0)
+        #std += torch.mean(torch.std(data, 1), 1)
         nb_samples += batch_samples
-        if i == 10:
-          break
     
     mean /= nb_samples
     std /= nb_samples
@@ -51,10 +74,11 @@ if __name__ == "__main__":
     base = "/hdd/trainval/" # Change this to point to your datapath
 
     print('--- dataset creator ---')
-    trainval_carData = CarDataset(base)
+    item_names = get_all_image_label_pairs(base)
+    carData = CarDataset(item_names)
     
     print('--- mean + std calc ---')
-    train_loader = _get_dataloader(10, trainval_carData)
+    train_loader = _get_dataloader(10, carData)
     mean, std = compute_mean_std(train_loader)
     print(mean)
     print(std)
